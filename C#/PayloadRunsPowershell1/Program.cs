bf1deb85baf5b6dbaac028f8ee5378b3cbb445a5
NullPtrStack <chang.102@gmail.com>
2019-10-16 21:21:51 -0700

    Create Program.cs
    init

---
+++ C#/PayloadRunsPowershell1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//new project -> C# Console App (.NET Framework) -> .Net 4.0
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This runs powershell script:\n" + System.AppDomain.CurrentDomain.BaseDirectory + "payload.ps1");
+            string strCmdText;
+            strCmdText = "-executionpolicy bypass -file " + System.AppDomain.CurrentDomain.BaseDirectory + "payload.ps1";
+            System.Diagnostics.Process.Start("powershell.exe", strCmdText);
+            Console.WriteLine("\n\nEnd of program\nCurrent Folder: " + System.AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine("Run:\npowershell " + strCmdText);
+        }
+    }
+}
